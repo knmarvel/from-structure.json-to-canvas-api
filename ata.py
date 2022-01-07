d61af271573e1bb7e7c088f7dc70c57dab3eb3ef
Kano Marvel <knmarvel@gmail.com>
2022-01-06 19:00:46 -0500

    fixed the url linking typos

--- ata.py
+++ ata.py
@@ -7,8 +7,8 @@ from slugify import slugify
 
 # download your structure.json and put the link below
 json_file = ""
-# get your course number from canvas
-course_number = "230"
+# get your course number from canvas, make sure you use it as a string
+course_number = ""
 # get your access token from https://my.kenzie.academy/profile/settings
 my_canvas_access_token = ""
 # ask kenzie studio for the subdomain
@@ -28,27 +28,18 @@ for mod in modules:
                 if "pages" in s.keys():
                     pages_list = pages_list + s["pages"]
 
-
-def request_maker(title, path):
-    page_json = {
-        "url": slugify(title),
-        "title": title,
-        "hide_from_students": True,
-        "editing_roles": "teachers",
-        "body": f"<p><iframe id='gatsby' style='width: 100%; height: 1024px;' src='{subdomain}.kenzie.studio{path}' width='100%' height='1024px' allowfullscreen='allowfullscreen' webkitallowfullscreen='webkitallowfullscreen' mozallowfullscreen='mozallowfullscreen'></iframe></p>",
-        "published": False,
-        "front_page": False,
-        "locked_for_user": False,
-        }
-    return json.dumps(page_json)
-
-
-page_requests = []
 for page in pages_list:
     if type(page) != "string":
+        token = my_canvas_access_token
         URL = f"https://my.kenzie.academy/api/v1/courses/{course_number}/pages/{slugify(page['title'])}"
-        print(URL)
-        request_body = request_maker(page['title'], page['path'])
-        request_header = {"Authorization": f"Bearer {my_canvas_access_token}"}
-        r = requests.put(URL, headers=request_header, data=request_body)
-        print(r.status_code)
+        path = page['path']
+        request_header = {"Authorization": f"Bearer {token}"}
+        r = requests.put(URL, headers=request_header, json={
+            'wiki_page':
+                {
+                    'title': page['title'],
+                    'body': f'<p><iframe style="width: 100%; height: 1024px;" src="https://{subdomain}.kenzie.studio{path}/index.html" width="100%" height="1024px" allowfullscreen="allowfullscreen" webkitallowfullscreen="webkitallowfullscreen" mozallowfullscreen="mozallowfullscreen"></iframe></p>'
+                }
+            })
+        print(r)
+
